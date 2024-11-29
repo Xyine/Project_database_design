ff0cbfd9d7e69b468d36bc6991cf2f74ff6912c3
Otabek Abdusattorov <abdusattorovotabek19@gmail.com>
2024-11-29 17:17:54 +0900

    Test sqlite3 connection

---
+++ main.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def connect_db():
+    connection = sqlite3.connect('f1.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    print(cursor.fetchall())
+    cursor.close()
+    connection.close()
+
+connect_db()
